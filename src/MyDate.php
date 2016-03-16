b1a5ddddb9e2324e65dd72c0666f6b9eb41292f9
Mihai Bucse <mbucse@mediamath.com>
2016-03-16 21:59:19 +0000

    convert files to conform psr-2

--- src/MyDate.php
+++ src/MyDate.php
@@ -1,18 +1,20 @@
 <?php
 
-  class MyDate {
+class MyDate
+{
 
-    public static function diff($start, $end) {
+    public static function diff($start, $end)
+    {
 
-      // Sample object:
-      return (object)array(
-        'years' => null,
-        'months' => null,
-        'days' => null,
-        'total_days' => null,
-        'invert' => null
-      );
+        // Sample object:
+        return (object)array(
+            'years' => null,
+            'months' => null,
+            'days' => null,
+            'total_days' => null,
+            'invert' => null
+        );
 
     }
 
-  }
+}
